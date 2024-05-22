cea27c59bf24f650e006237902e0c76c01befb02
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2024-05-22 20:33:41 -0300

    page To-do  created

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,11 +1,82 @@
 <template>
-  <q-page padding>
+  <q-page>
     <p>Page Todo</p>
+    <q-list>
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        :class="task.completed ? 'bg-green-1' : 'bg-orange-1'"
+        clickable
+        v-ripple
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label :class="{ 'task-done': task.completed }">
+            {{ task.name }}
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon name="event" size="18px" class="q-mr-xs" />
+            </div>
+
+            <div class="column">
+              <q-item-label class="row justify-end" caption>
+                {{ task.dueDate }}
+              </q-item-label>
+              <q-item-label class="row justify-end" caption>
+                {{ task.dueTime }}
+              </q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
+<!--
+        -->
 <script>
 export default {
-  // name: 'PageName',
-}
+  name: "PageTodo",
+  data() {
+    return {
+      tasks: [
+        {
+          id: "1",
+          name: "tarefa 1",
+          dueDate: null,
+          dueTime: null,
+          completed: false,
+        },
+        {
+          id: "2",
+          name: "tarefa 2",
+          dueDate: null,
+          dueTime: null,
+          completed: false,
+        },
+        {
+          id: "3",
+          name: "tarefa 3",
+          dueDate: null,
+          dueTime: null,
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
+<style scoped>
+.task-done {
+  text-decoration: line-through;
+}
+</style>
