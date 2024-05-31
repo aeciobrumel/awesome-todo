e755798392176af8fd5e570dc33de46b54d7c4c2
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2024-05-31 19:28:56 -0300

    +

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,11 +1,6 @@
 <template>
   <q-page>
-<<<<<<< HEAD
-    <p>Page Todo</p>
-    <q-list>
-=======
     <q-list bordered separator>
->>>>>>> 77c23b5b239a30d971d737d7d92bdb15861fa51a
       <q-item
         v-for="task in tasks"
         :key="task.id"
@@ -19,25 +14,16 @@
         </q-item-section>
 
         <q-item-section>
-<<<<<<< HEAD
           <q-item-label :class="{ 'task-done': task.completed }">
             {{ task.name }}
-=======
-          <q-item-label
-            :class="{ 'task-done' : task.completed}"
-          >
-            {{task.name}}
->>>>>>> 77c23b5b239a30d971d737d7d92bdb15861fa51a
           </q-item-label>
         </q-item-section>
 
         <q-item-section side>
           <div class="row">
             <div class="column justify-center">
-<<<<<<< HEAD
               <q-icon name="event" size="18px" class="q-mr-xs" />
             </div>
-
             <div class="column">
               <q-item-label class="row justify-end" caption>
                 {{ task.dueDate }}
@@ -45,17 +31,6 @@
               <q-item-label class="row justify-end" caption>
                 {{ task.dueTime }}
               </q-item-label>
-=======
-              <q-icon name="event" size="18px" class="q-mr-xs"/>
-            </div>
-            <div class="column">
-                <q-item-label class="row justify-end" caption>
-                  {{ task.dueDate }}
-                </q-item-label>
-                <q-item-label class="row justify-end" caption>
-                  {{ task.dueTime }}
-                </q-item-label>
->>>>>>> 77c23b5b239a30d971d737d7d92bdb15861fa51a
             </div>
           </div>
         </q-item-section>
@@ -68,66 +43,35 @@
         -->
 <script>
 export default {
-<<<<<<< HEAD
   name: "PageTodo",
-=======
-  name: 'PageTodo',
->>>>>>> 77c23b5b239a30d971d737d7d92bdb15861fa51a
   data() {
     return {
       tasks: [
         {
-<<<<<<< HEAD
           id: "1",
-          name: "tarefa 1",
-          dueDate: null,
-          dueTime: null,
+          name: "Ir ao shopping",
+          dueDate: "22/05/2024",
+          dueTime: "18:30",
           completed: false,
         },
         {
           id: "2",
-          name: "tarefa 2",
-          dueDate: null,
-          dueTime: null,
+          name: "Comprar bananas",
+          dueDate: "22/05/2024",
+          dueTime: "19:00",
           completed: false,
         },
         {
           id: "3",
-          name: "tarefa 3",
-          dueDate: null,
-          dueTime: null,
+          name: "Comprar maçã",
+          dueDate: "22/05/2024",
+          dueTime: "19:00",
           completed: false,
         },
       ],
     };
   },
 };
-=======
-          id: '1',
-          name: 'Ir ao shopping',
-          dueDate: '22/05/2024',
-          dueTime: '18:30',
-          completed: false
-        },
-        {
-          id: '2',
-          name: 'Comprar bananas',
-          dueDate: '22/05/2024',
-          dueTime: '19:00',
-          completed: false
-        },
-        {
-          id: '3',
-          name: 'Comprar maçã',
-          dueDate: '22/05/2024',
-          dueTime: '19:00',
-          completed: false
-        }
-      ]
-    }
-  }
-}
->>>>>>> 77c23b5b239a30d971d737d7d92bdb15861fa51a
 </script>
 <style scoped>
 .task-done {
